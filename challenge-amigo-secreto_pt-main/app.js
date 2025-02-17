c28d29c6a3aae4de388f399e967d3d2d438b5446
Barokkj <guisilveirared@gmai.com>
2025-02-16 22:11:42 -0300

    vs.001

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,42 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let arrayAmigos = []; 
+
+  
+function adicionarAmigo() {
+    let nome = document.querySelector("input").value; 
+         if (nome === "" ){
+    alert(
+        "Por favor, insira um nome."
+    )   ;
+}       
+        else{
+    arrayAmigos.push(nome); 
+    
+}
+       atualizarListaAmigos()
+        console.log(arrayAmigos); 
+        limparCampo()
+}
+
+function limparCampo() {
+    nome= document.querySelector("input");
+    nome.value= "";
+}
+
+function atualizarListaAmigos() {
+    const listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = ""; 
+
+    arrayAmigos.forEach((arrayAmigos) => {
+        const li = document.createElement("li");
+        li.textContent = arrayAmigos;
+        listaAmigos.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (arrayAmigos.length===0){
+        alert("Adicione amigos para poder sortear");
+    }
+    let sorteio = arrayAmigos[Math.floor(Math.random() * arrayAmigos.length)];
+    listaAmigos.innerHTML= sorteio
+}
